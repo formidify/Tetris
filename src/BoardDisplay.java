44eaffcd8dfc2bfbdf9bd0c4384ac48ea548d6b1
chens5 <chens5@cmc306-05.mathcs.carleton.edu>
2017-11-06 21:12:23 -0600

    Started the board display object

---
+++ src/BoardDisplay.java
@@ -0,0 +1,45 @@
+/**
+ * Created by chens5 on 11/6/17.
+ */
+public class BoardDisplay {
+    Board gameBoard;
+
+    public BoardDisplay( Board board){
+        gameBoard = board;
+
+    }
+
+    public void convertToGraphic(){
+        /*
+        1. find out which squares in the matrix are different
+        2. updating the new squares in the matrix
+        3. figure out which NxN pixel block to color
+        4. display the colored blocks on the screen.
+         */
+
+    }
+
+    private boolean isChanged(){
+
+    }
+
+    private void updateNewSquares(){
+
+    }
+
+    private void colorSquares(){
+
+    }
+
+    private displayBlocks(){
+
+    }
+
+    public void startDisplay(){
+
+    }
+
+    public void endDisplay(){
+
+    }
+}
