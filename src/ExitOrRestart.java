0b71984c32c9aaf650dcf3bd260e94dbcd04eec9
yuqimeng <qimeng.yu@outlook.com>
2017-11-08 20:09:40 -0600

    Add files via upload
    start game and new game/exit windows plus button icons

---
+++ src/ExitOrRestart.java
@@ -0,0 +1,101 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ * Created by yuq on 11/7/17.
+ */
+
+
+public class ExitOrRestart extends Application {
+    public static final double MIN_BUTTON_WIDTH = 30;
+    private Text directions = new Text("Would you like to start a new game?");
+
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        Node buttonPane = addButtons();
+        Node directionsPane = addText(directions, FontWeight.NORMAL, 14);
+
+        root.setTop(directionsPane);
+        root.setCenter(buttonPane);
+
+        Scene scene = new Scene(root, 350, 250);
+        primaryStage.setTitle("new game or exit - Tetris");
+        primaryStage.setResizable(false);
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    private Node addButtons() {
+        GridPane buttonPane = new GridPane();
+        buttonPane.setAlignment(Pos.CENTER);
+        buttonPane.setHgap(20);
+        buttonPane.setPadding(new Insets(0, 10, 0, 10));
+
+        // Create restart and exit buttons and add them to the grid
+        Button newGame = new Button();
+        Button exit = new Button();
+
+        buttonPane.add(newGame, 0, 0);
+        buttonPane.add(exit, 2, 0);
+
+        // create icon for new game button, add popup tooltip
+        Image imageNewG = new Image(getClass().getResourceAsStream("newGame.png"));
+        ImageView newgameView = new ImageView(imageNewG);
+        newgameView.setFitHeight(60);
+        newgameView.setFitWidth(60);
+        newGame.setGraphic(newgameView);
+        newGame.setTooltip(new Tooltip("Start a new game!"));
+
+        // create icon for exit button, add popup tooltip
+        Image imageExit = new Image(getClass().getResourceAsStream("exit.png"));
+        ImageView exitView = new ImageView(imageExit);
+        exitView.setFitHeight(60);
+        exitView.setFitWidth(60);
+        exit.setGraphic(exitView);
+        exit.setTooltip(new Tooltip("Exit!"));
+
+        exit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+
+        return buttonPane;
+    }
+
+    // TODO: THIS FUNCTION SHOULD GO SOMEWHERE ELSE! IT'S USEFUL BUT REDUNDANT!
+    private Node addText(Text text, FontWeight fontWeight, int fontSize) {
+        FlowPane flowPane = new FlowPane();
+        flowPane.setPrefHeight(100);
+        flowPane.setPadding(new Insets(80, 10, 0, 10));
+        flowPane.setAlignment(Pos.CENTER);
+        text.setFont(Font.font("Chalkduster", fontWeight, fontSize));
+        flowPane.getChildren().add(text);
+        return flowPane;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/StartGame.java
@@ -0,0 +1,103 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ * Created by yuq on 11/7/17.
+ */
+
+
+public class StartGame extends Application {
+    public static final double MIN_BUTTON_WIDTH = 30;
+    private Text directions = new Text("TETRIS");
+
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+        Node buttonPane = addButtons();
+        Node directionsPane = addText(directions, FontWeight.BOLD, 60);
+
+        root.setTop(directionsPane);
+        root.setCenter(buttonPane);
+
+        Scene scene = new Scene(root, 350, 450);
+        primaryStage.setTitle("New Game - Tetris");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    private Node addButtons() {
+        GridPane buttonPane = new GridPane();
+        buttonPane.setAlignment(Pos.CENTER);
+        buttonPane.setHgap(20);
+        buttonPane.setVgap(20);
+        buttonPane.setPadding(new Insets(0, 10, 0, 10));
+        //buttonPane.setStyle("-fx-background-image: url('images/background.png');-fx-background-size: stretch;-fx-background-position:center top;");
+
+        // Create start, setting, help buttons and add them to the grid
+        Button play = new Button();
+        Button settings = new Button();
+        Button help = new Button();
+
+        buttonPane.add(play, 0, 0, 2,2);
+        buttonPane.add(settings, 0, 2);
+        buttonPane.add(help, 1, 2);
+
+        // create icon for play button, set its size, add popup tooltip
+        play.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        Image imagePlay = new Image(getClass().getResourceAsStream("play.png"));
+        ImageView playView = new ImageView(imagePlay);
+        playView.setFitHeight( 2 * MIN_BUTTON_WIDTH );
+        playView.setFitWidth( 2 * MIN_BUTTON_WIDTH );
+        play.setGraphic(playView);
+        play.setTooltip(new Tooltip("Play a new game!"));
+
+        // create icon for settings button, set its size, add popup tooltip
+        Image imageSettings = new Image(getClass().getResourceAsStream("settings.png"));
+        ImageView settingsView = new ImageView(imageSettings);
+        settingsView.setFitHeight(MIN_BUTTON_WIDTH);
+        settingsView.setFitWidth(MIN_BUTTON_WIDTH);
+        settings.setGraphic(settingsView);
+        settings.setTooltip(new Tooltip("settings"));
+
+        // create icon for help button, set its size, add popup tooltip
+        Image imageHelp = new Image(getClass().getResourceAsStream("help.png"));
+        ImageView helpView = new ImageView(imageHelp);
+        helpView.setFitHeight(MIN_BUTTON_WIDTH);
+        helpView.setFitWidth(MIN_BUTTON_WIDTH);
+        help.setGraphic(helpView);
+        help.setTooltip(new Tooltip("help"));
+
+        return buttonPane;
+    }
+
+    private Node addText(Text text, FontWeight fontWeight, int fontSize) {
+        FlowPane flowPane = new FlowPane();
+        flowPane.setPrefHeight(100);
+        flowPane.setPadding(new Insets(80, 10, 0, 10));
+        flowPane.setAlignment(Pos.CENTER);
+        text.setFont(Font.font("Chalkduster", fontWeight, fontSize));
+        flowPane.getChildren().add(text);
+        return flowPane;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
